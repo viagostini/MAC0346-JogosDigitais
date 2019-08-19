3015e8afbd6919465f15bb85ff17f44b5e2ee05b
mergipe <gustavo.maciel@usp.br>
2019-08-19 13:43:38 -0300

    Added Unit and Weapon classes

---
+++ EP1/unit.lua
@@ -0,0 +1,24 @@
+local Unit = {}
+Unit.__index = Unit
+
+setmetatable(Unit, {
+    __call = function(cls, ...)
+        local self = setmetatable({}, cls)
+        self:_init(...)
+        return self
+    end,
+})
+
+function Unit:_init(name, attr_tbl, weapon_attr_tbl)
+    self.name = name
+    self.HP = attr_tbl.hp
+    self.str = attr_tbl.str
+    self.mag = attr_tbl.mag
+    self.skl = attr_tbl.skl
+    self.spd = attr_tbl.spd
+    self.lck = attr_tbl.lck
+    self.def = attr_tbl.def
+    self.res = attr_tbl.res
+    self.trait = attr_tbl.trait
+    self.weapon = Weapon(attr_tbl.weapon, weapon_attr_tbl)
+end
---
+++ EP1/weapon.lua
@@ -0,0 +1,20 @@
+local Weapon = {}
+Weapon.__index = Weapon
+
+setmetatable(Weapon, {
+    __call = function(cls, ...)
+        local self = setmetatable({}, cls)
+        self:_init(...)
+        return self
+    end,
+})
+
+function Weapon:_init(name, attr_tbl)
+    self.name = name
+    self.mt = attr_tbl.mt
+    self.hit = attr_tbl.hit
+    self.crt = attr_tbl.crt
+    self.wt = attr_tbl.wt
+    self.kind = attr_tbl.kind
+    self.eff = attr_tbl.eff
+end
