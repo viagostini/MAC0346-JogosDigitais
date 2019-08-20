037c7285315d59e247eb9e312c2e8a96a330bcbe
mergipe <gustavo.maciel@usp.br>
2019-08-20 10:44:28 -0300

    Triangle bonus datafile new format

--- EP1/triangle_bonus.lua
+++ EP1/triangle_bonus.lua
@@ -1,41 +1,65 @@
-local triangle_bonus = {}
-
-enum = {
-  ["sword"] = 1,
-  ["axe"] = 2,
-  ["lance"] = 3,
-  ["wind"] = 4,
-  ["thunder"] = 5,
-  ["fire"] = 6
+return {
+    sword = {
+        sword = 0,
+        axe = 1,
+        lance = -1,
+        bow = 0,
+        wind = 0,
+        thunder = 0,
+        fire = 0,
+    },
+    axe = {
+        sword = -1,
+        axe = 0,
+        lance = 1,
+        bow = 0,
+        wind = 0,
+        thunder = 0,
+        fire = 0
+    },
+    lance = {
+        sword = 1,
+        axe = -1,
+        lance = 0,
+        bow = 0,
+        wind = 0,
+        thunder = 0,
+        fire = 0
+    },
+    bow = {
+        sword = 0,
+        axe = 0,
+        lance = 0,
+        bow = 0,
+        wind = 0,
+        thunder = 0,
+        fire = 0
+    },
+    wind = {
+        sword = 0,
+        axe = 0,
+        lance = 0,
+        bow = 0,
+        wind = 0,
+        thunder = 1,
+        fire = -1
+    },
+    thunder = {
+        sword = 0,
+        axe = 0,
+        lance = 0,
+        bow = 0,
+        wind = -1,
+        thunder = 0,
+        fire = 1
+    },
+    fire = {
+        sword = 0,
+        axe = 0,
+        lance = 0,
+        bow = 0,
+        wind = 1,
+        thunder = -1,
+        fire = 0
+    }
 }
-
-kinds = {
-    ["sword"] = "physical",
-    ["axe"] = "physical",
-    ["lance"] = "physical",
-    ["wind"] = "magical",
-    ["thunder"] = "magical",
-    ["fire"] = "magical"
-}
-
-for i=1,6 do
-  triangle_bonus[i] = {}
-  for j=1,6 do
-    triangle_bonus[i][j] = 0
-  end
-end
-
-triangle_bonus[1][2] = 1
-triangle_bonus[1][3] = -1
-triangle_bonus[2][1] = -1
-triangle_bonus[2][2] = 1
-triangle_bonus[3][1] = 1
-triangle_bonus[3][2] = -1
-triangle_bonus[4][5] = 1
-triangle_bonus[4][6] = -1
-triangle_bonus[5][4] = -1
-triangle_bonus[5][6] = 1
-triangle_bonus[6][4] = 1
-triangle_bonus[6][5] = -1
-
-return triangle_bonus
