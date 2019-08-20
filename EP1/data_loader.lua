376b87832e17546a6c15f5648518dbc231857062
mergipe <gustavo.maciel@usp.br>
2019-08-20 10:45:23 -0300

    Loader module added

---
+++ EP1/data_loader.lua
@@ -0,0 +1,24 @@
+local DataLoader = {}
+
+local Weapon = require "weapon"
+local Unit = require "unit"
+
+function DataLoader.load_file(filename)
+    local path = debug.getinfo(1, 'S').source
+    path = path:match(".*/"):sub(2)
+    local chunk = assert(loadfile(path .. filename))
+    setfenv(chunk, {})
+    return chunk()
+end
+
+function DataLoader.load_units(input)
+    units = {}
+
+    for k, v in pairs(input.units) do
+        units[k] = Unit(k, v, input.weapons[v.weapon])
+    end
+
+    return units
+end
+
+return DataLoader
