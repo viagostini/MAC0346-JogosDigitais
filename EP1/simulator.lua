c698b146b5d8bf1fb7026012e0974eab1ab24da1
mergipe <gustavo.maciel@usp.br>
2019-08-20 11:46:56 -0300

    Simulator module completed; HP update missing

--- EP1/simulator.lua
+++ EP1/simulator.lua
@@ -25,9 +25,13 @@ end
 local function do_hit(attacker, defender, triangle_bonus)
     local acc = attacker.weapon.hit + 2 * attacker.skl + attacker.lck +
         10 * triangle_bonus[attacker.weapon.kind][defender.weapon.kind]
-    local avo = 2 * attacker.atkspd + attacker.lck
+    local avo = 2 * defender.atkspd + defender.lck
     local hit_chance = math.max(0, math.min(100, acc - avo))
-    local rand = math.floor((math.random(0, 100) + math.random(0, 100)) / 2)
+    local r1 = math.random(100)
+    local r2 = math.random(100)
+    local rand = (r1 + r2) / 2
+    print("\t\tr1=" .. r1 .. " r2=" .. r2 .. " rand=".. rand .. " hit_chance="
+          .. hit_chance)
 
     return rand <= hit_chance
 end
@@ -36,7 +40,8 @@ local function calculate_critical_bonus(attacker, defender)
     local critical_rate = attacker.weapon.crt + math.floor(attacker.skl / 2)
     local dodge = defender.lck
     local critical_chance = math.max(0, math.min(100, critical_rate - dodge))
-    local rand = math.random(0, 100)
+    local rand = math.random(100)
+    print("\t\trand=" .. rand .. " critical_chance=" .. critical_chance)
 
     if rand <= critical_chance then return 3
     else return 1
@@ -44,16 +49,51 @@ local function calculate_critical_bonus(attacker, defender)
 end
 
 local function calculate_damage(attacker, defender, critical_bonus)
+    local eff_bonus = 1
+    local attacker_attr = 0
+    local defender_attr = 0
+    
+    if attacker.eff ~= nil and defender.trait ~= nil and
+       attacker.eff == defender.trait then
+        eff_bonus = 2
+    end
+
+    if attacker.weapon.dmgtype == "physical" then
+        attacker_attr = attacker.str
+        defender_attr = defender.def
+    elseif attacker.weapon.dmgtype == "magical" then
+        attacker_attr = attacker.mag
+        defender_attr = defender.res
+    end
+
+    local power = attacker_attr + eff_bonus * (attacker.weapon.mt +
+                  triangle_bonus[attacker.weapon.kind][defender.weapon.kind])
+    local damage = critical_bonus * (power - defender_attr)
+    
+    return damage
 end
 
 local function attack(attacker, defender, triangle_bonus)
+    print("\t" .. attacker.name .. " attacking " .. defender.name)
+
+    if attacker.HP == 0 then
+        return
+    end
+
     if do_hit(attacker, defender, triangle_bonus) then
         local critical_bonus = calculate_critical_bonus(attacker, defender)
         local damage = calculate_damage(attacker, defender, critical_bonus)
+        print("\t\tDamage dealt: " .. damage)
+        defender.HP = math.max(0, defender.HP - damage)
+    else
+        print("\t\tAttack missed")
     end
 end
 
-local function init_fight(attacker, defender, triangle_bonus)
+local function init_fight(fight_nb, attacker, defender, triangle_bonus)
+    print("Luta " .. fight_nb .. ": " .. attacker.name .. " vs " ..
+          defender.name)
+
     attack(attacker, defender, triangle_bonus)
     attack(defender, attacker, triangle_bonus)
 
@@ -70,7 +110,7 @@ function SIMULATOR.run(scenario_input)
 
     for fight_nb, fight in ipairs(scenario_input.fights) do
         local attacker, defender = get_fighters(fight, units)
-        init_fight(attacker, defender, triangle_bonus)
+        init_fight(fight_nb, attacker, defender, triangle_bonus)
     end
 
     scenario_output = set_output(scenario_input, units)
