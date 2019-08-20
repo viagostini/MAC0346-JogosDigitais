a19c2d262e3580d4e9c617714e17227998652442
mergipe <gustavo.maciel@usp.br>
2019-08-20 11:45:45 -0300

    Weapon dmgtype implemented

--- EP1/weapon.lua
+++ EP1/weapon.lua
@@ -20,6 +20,12 @@ function Weapon:_init(name, attr_tbl)
     self.wt = attr_tbl.wt
     self.kind = attr_tbl.kind
     self.eff = attr_tbl.eff
+
+    if physical[self.kind] then
+        self.dmgtype = "physical"
+    elseif magical[self.kind] then
+        self.dmgtype = "magical"
+    end
 end
 
 return Weapon
