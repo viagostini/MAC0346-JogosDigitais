8f6ee074683c235d05650a6fe8d1631ca43ef3f8
mergipe <gustavo.maciel@usp.br>
2019-08-28 14:30:02 -0300

    Considering only one tileset

--- EP2/src/AssetManager.lua
+++ EP2/src/AssetManager.lua
@@ -2,39 +2,33 @@ local AssetManager = {}
 
 local maps_path = "/maps/"
 local map
-local tilesets
+local tileset
 
-local function setup_tilesets()
-    tilesets = map.tilesets
-
-    for _, v in ipairs(tilesets) do
-        v.displaywidth = math.floor(love.graphics.getWidth() / map.width)
-        v.displayheight = math.floor(love.graphics.getHeight() / map.height)
-        v.imageobj = love.graphics.newImage(maps_path .. v.image)
-        v.quads = setmetatable({}, {__index = function(t, k)
-            t[k] = love.graphics.newQuad(
-            )
-        end})
-        v.batch = love.graphics.newSpriteBatch(
-            v.imageobj,
-            v.displaywidth * v.displayheight
+local function setup_tileset()
+    tileset = map.tilesets[1]
+    tileset.displaywidth = math.floor(love.graphics.getWidth() / map.width)
+    tileset.displayheight = math.floor(love.graphics.getHeight() / map.height)
+    tileset.imageobj = love.graphics.newImage(maps_path .. tileset.image)
+    tileset.quads = setmetatable({}, {__index = function(t, k)
+        t[k] = love.graphics.newQuad(
         )
-    end
+    end})
+    tileset.batch = love.graphics.newSpriteBatch(
+        tileset.imageobj,
+        tileset.displaywidth * tileset.displayheight
+    )
 end
 
 function AssetManager.init(map_name)
     local chunk = love.filesystem.load(maps_path .. map_name .. ".lua")
     map = chunk()
-    setup_tilesets()
+    setup_tileset()
     love.graphics.setBackgroundColor(map.backgroundcolor[1],
                                      map.backgroundcolor[2],
                                      map.backgroundcolor[3])
 end
 
 function AssetManager.draw()
-    for _, v in ipairs(tilesets) do
-        love.graphics.draw(v.batch, v.tilewidth, v.tileheight, 0, 0, 0)
-    end
     love.graphics.print("FPS: " .. love.timer.getFPS(), 0, 0)
 end
 
