ec03aaf88bc46563ceb5ceba33c45da1dde3f208
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2019-08-28 15:15:24 -0300

    Fixed command line arg position

--- EP2/main.lua
+++ EP2/main.lua
@@ -2,7 +2,7 @@ local AssetManager = require("src/AssetManager")
 
 function love.load(arg)
     love.graphics.setColor(0, 0, 0)
-    AssetManager.init(arg[2])
+    AssetManager.init(arg[1])
 end
 
 function love.update(dt)
